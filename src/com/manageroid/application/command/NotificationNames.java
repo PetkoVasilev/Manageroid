9c0bbf13ecdf3cfc40aac9120a094d572c5047fb
kkirilov <kiril.kirilov.1985@abv.bg>
2011-02-02 00:46:23 +0200

    remove redundant files

--- src/com/manageroid/application/command/NotificationNames.java
+++
@@ -1,6 +0,0 @@
-package com.manageroid.application.command;
-
-public class NotificationNames {
-	public static final String STARTUP_MAINMENU = "startMainMenu";
-	public static final String REFRESH_MAINMENU = "refreshMainMenu";
-}
--- src/com/manageroid/application/proxy/ProxyNames.java
+++
@@ -1,7 +0,0 @@
-package com.manageroid.application.proxy;
-
-public class ProxyNames {
-
-	public static final String TASKS_PROXY = "tasksProxy";
-	//public static final String CURRENCY_PROXY = "currencyProxy";
-}
